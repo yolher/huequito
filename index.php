0e9829eebb864933811c2bdf9e081586e376501c
yolher <yocahe5@gmail.com>
2014-11-26 00:39:44 -0500

    actualizacion-responsive

--- index.php
+++ index.php
@@ -12,50 +12,189 @@
 	<link rel="stylesheet" href="css/normalize.css">
 	<script src="js/jquery-1.7.2.min.js"></script>
 	<script src="js/script.js"></script>
+  <link rel="stylesheet" href="css/font-awesome.min.css">
+  <link rel="stylesheet" href="css/stylebanner.css">
 </head>
 
 
 <body class="home" id="Inicio">
 	<header>
 		<div id="up">
-
+			<div id="varios">
+				<div id="redes">
+					<ul>
+						<li><img src="images/contacto.png" alt=""></li>
+						<li><img src="images/correo.png" alt=""></li>
+						<li><img src="images/facebook.png" alt=""></li>
+					</ul>
+				</div>
+			</div>
 			
 		</div>
-		<div id="lnb">
-		<div id="logo">
-			
-		</div>
-		<nav>
-			<ul>
-				<li>Inicio</li>
-				<li>Productos</li>
-				<li>Nosotros</li>
-				<li>Coctactanos</li>
-			</ul>
-		</nav>
-		<div id="buscador">
-			
-		</div>
-		</div>
+			<div id="lnb">
+				<div id="logo">
+					<img src="images/logo.png" alt="">
+				</div>
+				<nav>
+					<ul>
+						<a href=""><li class="menu">INICIO</li></a>
+						<a href=""><li class="menu">CONOSCANOS</li></a>
+						<a href="">
+						<li class="menu">MODA
+							<ul>
+								<a href=""><li class="submenu">HOLA</li></a>
+								<a href=""><li class="submenu">HOLA</li></a>
+							</ul>
+						</li></a>
+						<a href=""><li class="menu">PROMOCIONES</li></a>
+						<a href="">
+						<li class="menu">PRODUCTOS
+							<ul>
+								<a href=""><li class="submenu">HOLA</li></a>
+								<a href=""><li class="submenu">HOLA</li></a>
+							</ul>
+						</li></a>
+						<a href=""><li class="menu">CONTACTOS</li></a>
+					</ul>
+				</nav>
+				<div id="buscador">
+					<input id="into" type="text" placeholder="Buscar" class="hasPlaceholder">
+					<input type="button" id="boton" value="Buscar">
+				</div>
+			</div>
 	</header>
 
 	<div id="container">
 		<section id="banner">
+		
+    <div id="slides">
+      <img src="images/example-slide-1.jpg" alt="Photo by: Missy S Link: http://www.flickr.com/photos/listenmissy/5087404401/">
+      <img src="images/example-slide-2.jpg" alt="Photo by: Daniel Parks Link: http://www.flickr.com/photos/parksdh/5227623068/">
+      <img src="images/example-slide-3.jpg" alt="Photo by: Mike Ranweiler Link: http://www.flickr.com/photos/27874907@N04/4833059991/">
+      <img src="images/example-slide-4.jpg" alt="Photo by: Stuart SeegerLink: http://www.flickr.com/photos/stuseeger/97577796/">
+    </div>
+
+  <!-- End SlidesJS Required: Start Slides -->
+
+  <!-- SlidesJS Required: Link to jQuery -->
+  <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+  <!-- End SlidesJS Required -->
+
+  <!-- SlidesJS Required: Link to jquery.slides.js -->
+  <div id="paginas">	
+  <script src="js/jquery.slides.min.js"></script>
+  </div>
+  <!-- End SlidesJS Required -->
+
+  <!-- SlidesJS Required: Initialize SlidesJS with a jQuery doc ready -->
+  <script>
+    $(function() {
+      $('#slides').slidesjs({
+        width: 940,
+        height: 400,
+        play: {
+          active: true,
+          auto: true,
+          interval: 4000,
+          swap: true
+        }
+      });
+    });
+  </script>
 			
 		</section>
 		<section id="categorias">
-			<div class="contcat"></div>
-			<div class="contcat"></div>
-			<div class="contcat"></div>
+			<div class="contcat"><a href=""><img src="images/categoria1.png" alt=""></a></div>
+			<div class="contcat"><a href=""><img src="images/categoria2.png" alt=""></a></div>
+			<div class="contcat"><a href=""><img src="images/categoria3.png" alt=""></a></div>
 		</section>
 
 		<section id="productos">
 			<div class="prod">
 				<div class="imagen">
-					
+					<a href=""><img src="images/producto.png" alt=""></a>
 				</div>
 				<div class="descripcion">
-					
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nulla.
+					</p>
+				</div>
+
+			</div>
+				<div class="prod">
+				<div class="imagen">
+					<a href=""><img src="images/producto.png" alt=""></a>
+				</div>
+				<div class="descripcion">
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nulla.
+					</p>
+				</div>
+
+			</div>
+				<div class="prod">
+				<div class="imagen">
+					<a href=""><img src="images/producto.png" alt=""></a>
+				</div>
+				<div class="descripcion">
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nulla.
+					</p>
+				</div>
+
+			</div>
+				<div class="prod">
+				<div class="imagen">
+					<a href=""><img src="images/producto.png" alt=""></a>
+				</div>
+				<div class="descripcion">
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nulla.
+					</p>
+				</div>
+
+			</div>
+				<div class="prod">
+				<div class="imagen">
+					<a href=""><img src="images/producto.png" alt=""></a>
+				</div>
+				<div class="descripcion">
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nulla.
+					</p>
+				</div>
+
+			</div>
+				<div class="prod">
+				<div class="imagen">
+					<a href=""><img src="images/producto.png" alt=""></a>
+				</div>
+				<div class="descripcion">
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nulla.
+					</p>
+				</div>
+
+			</div>
+				<div class="prod">
+				<div class="imagen">
+					<a href=""><img src="images/producto.png" alt=""></a>
+				</div>
+				<div class="descripcion">
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nulla.
+					</p>
+				</div>
+
+			</div>
+				<div class="prod">
+				<div class="imagen">
+					<a href=""><img src="images/producto.png" alt=""></a>
+				</div>
+				<div class="descripcion">
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nulla.
+					</p>
 				</div>
 
 			</div>
@@ -64,7 +203,12 @@
 		
 	</div>
 	<footer>
-			
+			<div id="contenidof">
+				
+			</div>
+			<div id="derechosf">
+				
+			</div>
 	</footer>
 
 	
