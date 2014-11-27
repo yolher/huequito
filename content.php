0fbcac986e7d120227a6c2c702eb71a7b662bd60
yolher <yocahe5@gmail.com>
2014-11-27 13:14:27 -0500

    agregar archivos

---
+++ content.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Menu</title>
+	<script src="jquery.min.js"></script>
+	<script src="script.js"></script>
+</head>
+<body>
+	<?php 
+	require_once("modules/mod_menu/default.php");
+	require_once("modules/mod_content/default.php");
+	
+	//-- mostramos el menu
+	displayMenus();
+	
+
+	?>
+
+	
+</body>
+</html>
