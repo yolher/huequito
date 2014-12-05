e9bd89ad6f5fb98a64e8ccb7464e5c2252a2fdd8
oaguillondev <aguillon_@hotmail.com>
2014-12-05 09:47:31 -0500

    gallery internal page

--- modules/mod_content_int/default.php
+++ modules/mod_content_int/default.php
@@ -7,6 +7,8 @@
 
 		
 		?>
+		<script src="modules/mod_content_int/js/lytebox.js"></script>
+		<link href="modules/mod_content_int/css/lytebox.css" rel="stylesheet" />
 		<div class="titlePage">
 			<h1><?php echo $intems[0]["titulo_ent"]; ?></h1>
 		</div>
@@ -17,9 +19,32 @@
 			<div class="desc">
 				<?php echo $intems[0]["desc_ent"]; ?>	
 			</div>
-			<div class="galerias">
+			<?php 
+
+			if($intems[0]["gallery_ent"] != "null"){ ?>
+			<div id="galerias">
 				
-			</div>
+				<?php 
+				  $carpeta = "server/php/files/".$intems[0]["gallery_ent"];
+
+		          $filehandle = opendir($carpeta); // Abrir archivos
+		            echo "<ul>";
+		              while ($file = readdir($filehandle)) {
+		                      if ($file != "." && $file != ".." && $file != "." && $file != "Thumbs.db") {
+		                        $tamanyo = GetImageSize($carpeta .'/'. $file); 
+		                        $foto = $carpeta .'/'. $file;
+		                        //echo $imagen="<img src='".$foto."'>";
+		                        echo $imagen='<li><a href="'.$foto.'" class="lytebox" data-lyte-options="group:vacation"><img src="'.$foto.'" /></a></li>';
+		                        //echo $imagen="<li><a href='".$foto."'><img src='".$foto."'></a></li>";  
+		                      } 
+		              } 
+		              closedir($filehandle); // Fin lectura archivos  
+		            echo "</ul>";
+		         ?>
+
+		    </div>
+			<?php } ?>
+			
 		</div>
 		<?php	
 	}
