36186b0548e662303c277faddb4c4ad052da284a
oaguillondev <aguillon_@hotmail.com>
2014-12-04 19:16:26 -0500

    add sorry.php file and htaces redirection

---
+++ sorry.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Lo sentimos</title>
+</head>
+<body>
+	lo sentimos esta seccion no exite o esta en construcción!!!
+</body>
+</html>
